54439f2897fb9895ced14b700b70389750282543
Ravin Kumar Jangir <ravinjangir.rj1100@gmail.com>
2023-01-16 20:18:03 +0530

    prime problem is done

---
+++ prime.js
@@ -0,0 +1,14 @@
+let n=5;
+let count=0;
+
+for(let i=1; i<=n; i++){
+  if(n%i==0){
+    count++;
+  }
+}
+if(count=2){
+  console.log("Prime Number");
+}
+else{
+console.log("Not a Prime Number");
+}
